c8e66cf65655de3dbb2240791d5c3c542100afaa
Joao-Lucas-Pontes-Freitas <joaolucaspontesfreitas@ufu.br>
2025-07-19 15:08:37 -0300

    git nova

--- src/AutoSklearn/with_ensemble.py
+++ src/AutoSklearn/with_ensemble.py
@@ -1,10 +1,12 @@
-def without_ensemble_experiment(X_train, y_train, time_limit, dataset_name):
+def with_ensemble_experiment(X_train, y_train, time_limit, dataset_name, tmp_path):
     import autosklearn.classification
+
     automl = autosklearn.classification.AutoSklearnClassifier(
         time_left_for_this_task=time_limit,
-        tmp_folder="without_ensemble_tmp",
+        tmp_folder=tmp_path,
         # get_trials_callback= grava_json,
-        ensemble_size=1
+        seed=1,
+        memory_limit=4096,  # 4GB
     )
     automl.fit(X_train, y_train, dataset_name=dataset_name)
     return automl
--- src/AutoSklearn/without_ensemble.py
+++ src/AutoSklearn/without_ensemble.py
@@ -1,10 +1,13 @@
-def with_ensemble_experiment(X_train, y_train, time_limit, dataset_name):
+def without_ensemble_experiment(X_train, y_train, time_limit, dataset_name, tmp_path):
     import autosklearn.classification
 
     automl = autosklearn.classification.AutoSklearnClassifier(
         time_left_for_this_task=time_limit,
-        tmp_folder="with_ensemble_tmp",
+        tmp_folder=tmp_path,
+        ensemble_size=1,
         # get_trials_callback= grava_json,
+        seed=1,
+        memory_limit=4096,
     )
     automl.fit(X_train, y_train, dataset_name=dataset_name)
     return automl
