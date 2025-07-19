95331ce7a852eb0c7a1fd227e993c42e66f9f1fe
Joao-Lucas-Pontes-Freitas <joaolucaspontesfreitas@ufu.br>
2025-07-19 18:56:03 -0300

    f1

--- src/AutoSklearn/without_ensemble.py
+++ src/AutoSklearn/without_ensemble.py
@@ -1,5 +1,7 @@
 def without_ensemble_experiment(X_train, y_train, time_limit, dataset_name, tmp_path):
     import autosklearn.classification
+    import autosklearn.metrics
+    scorer = autosklearn.metrics.f1
 
     automl = autosklearn.classification.AutoSklearnClassifier(
         time_left_for_this_task=time_limit,
@@ -8,6 +10,7 @@ def without_ensemble_experiment(X_train, y_train, time_limit, dataset_name, tmp_
         # get_trials_callback= grava_json,
         seed=1,
         memory_limit=4096,
+        metric = scorer
     )
     automl.fit(X_train, y_train, dataset_name=dataset_name)
     return automl
