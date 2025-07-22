f6aa2d1684693389737bcb5b83457b95fe2dfcd2
Joao-Lucas-Pontes-Freitas <joaolucaspontesfreitas@ufu.br>
2025-07-21 23:07:02 -0300

    ajuste f1 score

--- src/AutoSklearn/with_ensemble.py
+++ src/AutoSklearn/with_ensemble.py
@@ -1,14 +1,13 @@
 def with_ensemble_experiment(X_train, y_train, time_limit, dataset_name, tmp_path):
     import autosklearn.classification
     import autosklearn.metrics
-    scorer = autosklearn.metrics.f1
+
+    scorer = autosklearn.metrics.f1_weighted
 
     automl = autosklearn.classification.AutoSklearnClassifier(
         time_left_for_this_task=time_limit,
         tmp_folder=tmp_path,
-        # get_trials_callback= grava_json,
         seed=1,
-        memory_limit=4096,  # 4GB
         metric=scorer
     )
     automl.fit(X_train, y_train, dataset_name=dataset_name)
--- src/AutoSklearn/without_ensemble.py
+++ src/AutoSklearn/without_ensemble.py
@@ -1,16 +1,15 @@
 def without_ensemble_experiment(X_train, y_train, time_limit, dataset_name, tmp_path):
     import autosklearn.classification
     import autosklearn.metrics
-    scorer = autosklearn.metrics.f1
+
+    scorer = autosklearn.metrics.f1_weighted
 
     automl = autosklearn.classification.AutoSklearnClassifier(
         time_left_for_this_task=time_limit,
         tmp_folder=tmp_path,
-        ensemble_size=1,
-        # get_trials_callback= grava_json,
         seed=1,
-        memory_limit=4096,
-        metric = scorer
+        metric=scorer,
+        ensemble_size=1
     )
     automl.fit(X_train, y_train, dataset_name=dataset_name)
     return automl
